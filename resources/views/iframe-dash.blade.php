831c10dd957872e8e75d4033a0cb13695e4aeac6
taliptako <talipdurmus1@gmail.com>
2017-08-02 19:37:16 +0300

    ads

--- resources/views/iframe-dash.blade.php
+++ resources/views/iframe-dash.blade.php
@@ -5,6 +5,12 @@
     <title>{{ $video->name }}</title>
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/video.js/6.1.0/video-js.min.css" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/5.0.3/videojs.ads.min.css" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/videojs-ima/0.6.0/videojs.ima.min.css" rel="stylesheet">
+
+
     <!-- If you'd like to support IE8 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/6.1.0/ie8/videojs-ie8.min.js"></script>
 </head>
@@ -30,8 +36,30 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dashjs/2.5.0/dash.all.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-dash/2.9.1/videojs-dash.min.js"></script>
 
+    <script src="//imasdk.googleapis.com/js/sdkloader/ima3.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/5.0.3/videojs.ads.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-ima/0.6.0/videojs.ima.min.js"></script>
+
     <script>
         var player = videojs('video');
+
+        var options = {
+            id: 'video',
+            adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator='
+        };
+
+        // This must be called before player.play() below.
+        player.ima(options);
+        player.ima.requestAds();
+        // On mobile devices, you must call initializeAdDisplayContainer as the result
+        // of a user action (e.g. button click). If you do not make this call, the SDK
+        // will make it for you, but not as the result of a user action. For more info
+        // see our examples, all of which are set up to work on mobile devices.
+        // player.ima.initializeAdDisplayContainer();
+
+        // This must be called after player.ima(...) above.
 
     </script>
 
