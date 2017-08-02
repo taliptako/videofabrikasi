7414d82271e33870e6714c45aea4935a7e4de928
taliptako <talipdurmus1@gmail.com>
2017-08-02 19:43:53 +0300

    ads

--- resources/views/iframe-dash.blade.php
+++ resources/views/iframe-dash.blade.php
@@ -52,14 +52,30 @@
 
         // This must be called before player.play() below.
         player.ima(options);
-        player.ima.requestAds();
-        // On mobile devices, you must call initializeAdDisplayContainer as the result
-        // of a user action (e.g. button click). If you do not make this call, the SDK
-        // will make it for you, but not as the result of a user action. For more info
-        // see our examples, all of which are set up to work on mobile devices.
-        // player.ima.initializeAdDisplayContainer();
 
-        // This must be called after player.ima(...) above.
+        // Remove controls from the player on iPad to stop native controls from stealing
+        // our click
+        var contentPlayer =  document.getElementById('video_html5_api');
+        if ((navigator.userAgent.match(/iPad/i) ||
+                navigator.userAgent.match(/Android/i)) &&
+            contentPlayer.hasAttribute('controls')) {
+            contentPlayer.removeAttribute('controls');
+        }
+
+        // Initialize the ad container when the video player is clicked, but only the
+        // first time it's clicked.
+        var startEvent = 'click';
+        if (navigator.userAgent.match(/iPhone/i) ||
+            navigator.userAgent.match(/iPad/i) ||
+            navigator.userAgent.match(/Android/i)) {
+            startEvent = 'touchend';
+        }
+
+        player.one(startEvent, function() {
+            player.ima.initializeAdDisplayContainer();
+            player.ima.requestAds();
+            player.play();
+        });
 
     </script>
 
