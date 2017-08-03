5465bd4c854399427c99f14c5447a57717bdf86d
taliptako <talipdurmus1@gmail.com>
2017-08-03 16:14:16 +0300

    test

--- resources/views/iframe-dash.blade.php
+++ resources/views/iframe-dash.blade.php
@@ -42,6 +42,8 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-ima/0.6.0/videojs.ima.min.js"></script>
 
+    <script src="{{ url('videojs-contrib-quality-levels.min.js') }}"></script>
+
     <script>
         var player = videojs('video');
 
@@ -50,6 +52,8 @@
             adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator='
         };
 
+        player.qualityLevels();
+        
         // This must be called before player.play() below.
         player.ima(options);
 
