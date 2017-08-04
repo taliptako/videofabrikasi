58f873bb268eab7751e8185bdc453ffafbb460ed
taliptako <talipdurmus1@gmail.com>
2017-08-04 17:51:09 +0300

    fix

--- app/Http/Controllers/IframeController.php
+++ app/Http/Controllers/IframeController.php
@@ -29,8 +29,8 @@ class IframeController extends Controller
             $skin_class = 'vjs-default-skin';
         }
 
-        $video->dash_url = "https://". $video->user->id .".b-cdn.net/". $video->id . "/" . $video->extension . "/dash.mpd";
-        $video->fallback_url = "https://". $video->user->id .".b-cdn.net/". $video->id . "/" . "/mp4/fallback_480p.mp4";
+        $video->dash_url = "https://user-". $video->user->id .".b-cdn.net/". $video->id . "/" . $video->extension . "/dash.mpd";
+        $video->fallback_url = "https://user-". $video->user->id .".b-cdn.net/". $video->id . "/" . "/mp4/fallback_480p.mp4";
 
         return view('iframe-dash', ['video' => $video,
             'skin_url'                      => $skin_url, 'skin_class' => $skin_class, ]);
