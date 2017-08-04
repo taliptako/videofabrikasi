29cf2424c46b8a83daa710814dbd41075b8c5bbd
taliptako <talipdurmus1@gmail.com>
2017-08-04 17:50:20 +0300

    cdn links

--- app/Http/Controllers/IframeController.php
+++ app/Http/Controllers/IframeController.php
@@ -11,7 +11,7 @@ class IframeController extends Controller
     public function show($id, $hash)
     {
         $video = Cache::remember('video-'.$id, 2, function () use ($id) {
-            return Video::where('status', 1)->with('setting')->findorfail($id);
+            return Video::where('status', 1)->with('setting', 'user')->findorfail($id);
         });
 
         if ($video->hash != $hash) {
@@ -29,8 +29,8 @@ class IframeController extends Controller
             $skin_class = 'vjs-default-skin';
         }
 
-        $video->dash_url = Storage::url($video->folder.'/'.$video->extension.'/dash.mpd');
-        $video->fallback_url = Storage::url($video->folder.'/mp4/fallback_480p.mp4');
+        $video->dash_url = "https://". $video->user->id .".b-cdn.net/". $video->id . "/" . $video->extension . "/dash.mpd";
+        $video->fallback_url = "https://". $video->user->id .".b-cdn.net/". $video->id . "/" . "/mp4/fallback_480p.mp4";
 
         return view('iframe-dash', ['video' => $video,
             'skin_url'                      => $skin_url, 'skin_class' => $skin_class, ]);
