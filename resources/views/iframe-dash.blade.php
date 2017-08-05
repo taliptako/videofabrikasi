03c418a5158031910a632bfe2934fd72b5f18298
taliptako <talipdurmus1@gmail.com>
2017-08-05 14:01:19 +0300

    vmap

--- resources/views/iframe-dash.blade.php
+++ resources/views/iframe-dash.blade.php
@@ -48,7 +48,7 @@
 
         var options = {
             id: 'video',
-            adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator='
+            adTagUrl: 'https://videofabrikasi.com/vmap.xml'
         };
 
         // This must be called before player.play() below.
