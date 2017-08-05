5bf1661925824f0cfae6026240defe0872493221
taliptako <talipdurmus1@gmail.com>
2017-08-05 17:47:19 +0300

    tes

--- resources/views/vmap.blade.php
+++ resources/views/vmap.blade.php
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <vmap:VMAP xmlns:vmap="http://www.iab.net/videosuite/vmap" version="1.0">
     <vmap:AdBreak timeOffset="start" breakType="linear" breakId="preroll">
-        <vmap:AdSource id="preroll-ad-1" skipoffset="00:00:05" allowMultipleAds="false" followRedirects="true">
+        <vmap:AdSource id="preroll-ad-1" allowMultipleAds="false" followRedirects="true">
             <vmap:AdTagURI templateType="vast3"><![CDATA[https://pubads.g.doubleclick.net/gampad/ads?sz=400x300|640x480&iu=/40888219/videojs_test2&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]]]></vmap:AdTagURI>
         </vmap:AdSource>
     </vmap:AdBreak>
     <vmap:AdBreak timeOffset="00:00:15.000" breakType="linear" breakId="midroll-1">
-        <vmap:AdSource id="midroll-1-ad-1" skipoffset="00:00:05" allowMultipleAds="false" followRedirects="true">
+        <vmap:AdSource id="midroll-1-ad-1" allowMultipleAds="false" followRedirects="true">
             <vmap:AdTagURI templateType="vast3"><![CDATA[https://pubads.g.doubleclick.net/gampad/ads?sz=400x300|640x480&iu=/40888219/videojs_test2&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]]]></vmap:AdTagURI>
         </vmap:AdSource>
     </vmap:AdBreak>
