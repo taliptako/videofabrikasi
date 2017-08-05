842c44a30a435e9648c345a9a0aa03759d84a103
taliptako <talipdurmus1@gmail.com>
2017-08-05 15:35:58 +0300

    cdn77

--- app/Http/Controllers/IframeController.php
+++ app/Http/Controllers/IframeController.php
@@ -28,8 +28,14 @@ class IframeController extends Controller
             $skin_class = 'vjs-default-skin';
         }
 
-        $video->dash_url = 'https://user'.$video->user->id.'.b-cdn.net/'.$video->id.'/'.$video->extension.'/dash.mpd';
-        $video->fallback_url = 'https://user'.$video->user->id.'.b-cdn.net/'.$video->id.'/'.'/mp4/fallback_480p.mp4';
+
+
+        $video->dash_url = 'https://1540476022.rsc.cdn77.org/'.$video->id.'/'.$video->extension.'/dash.mpd';
+        $video->fallback_url = 'https://1540476022.rsc.cdn77.org/'.$video->id.'/mp4/fallback_480p.mp4';
+
+
+        //$video->dash_url = 'https://user'.$video->user->id.'.b-cdn.net/'.$video->id.'/'.$video->extension.'/dash.mpd';
+        //$video->fallback_url = 'https://user'.$video->user->id.'.b-cdn.net/'.$video->id.'/mp4/fallback_480p.mp4';
 
         return view('iframe-dash', ['video' => $video,
             'skin_url'                      => $skin_url, 'skin_class' => $skin_class, ]);
